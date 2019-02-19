0aae350a45a0d2faad677d5bb7e570fbbd4f6ed3
fairyin <fairyin@126.com>
2019-02-19 12:15:05 +0800

    优化底部导航菜单。

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,22 @@
 import 'package:flutter/material.dart';
 import 'layout/bottom_navigation.dart';
+import 'package:flutter/services.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  runApp(new iLegsApp());
+}
+
+class iLegsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'iLegs',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.deepPurple,
       ),
       home: BottomNavigationWidget(),
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -2,21 +2,46 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget{
   @override
+
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("iLegs"),
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text("iLegs 最新发布专辑"),
+            backgroundColor: Colors.deepPurple
+        ),
+        body: new HomeBody(),
       ),
-      body: new Center(
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: (){
-        },
-        tooltip: '长按展示文字',
-        child: new Icon(Icons.add),
-        foregroundColor: Colors.white,//设置显示图标或者文本的颜色//前提是子child没有设置
-        backgroundColor: Colors.deepPurple,//设置按钮的背景色
-      ),
-     );
+    );
+  }
+}
+
+class HomeBody extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new GridViewState();
+}
+
+class GridViewState extends State {
+  @override
+  Widget build(BuildContext context) => new GridView.count(
+      primary: false,
+      padding: const EdgeInsets.only(top: 2.0),
+      mainAxisSpacing: 2.0,//竖向间距
+      crossAxisCount: 2,//横向Item的个数
+      crossAxisSpacing: 2.0,//横向间距
+      childAspectRatio: 2/3,
+      children: buildGridTileList(50));
+
+  List<Widget> buildGridTileList(int number) {
+    List<Widget> widgetList = new List();
+    for (int i = 0; i < number; i++) {
+      widgetList.add(getItemWidget());
+    }
+    return widgetList;
+  }
+  String url = "http://leg.imcn.vip/cover/20190215/5c66e59e95ae9.jpg-mobile_cover?e=1550565906&token=u6UtZg2ym_yzh2DfwJb4gRDwMqg-7oxG_eVtqP2w:aI_kq3NzyxMuPLg5L1LGpGdX7Jw=";
+  Widget getItemWidget() {
+    //BoxFit 可设置展示图片时 的填充方式
+    return new Image(image: new NetworkImage(url), fit: BoxFit.cover);
   }
 }
